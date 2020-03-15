fc83719ee5b0df91f2878c2fedf1883b0c2e3a02
Jayawardana JTH <tiran2323@gmail.com>
2020-03-15 18:41:05 +0530

    Create Activity to liner motion functions

---
+++ app/src/main/java/com/example/scienceguider/Liner_Motion_Functions.java
@@ -0,0 +1,14 @@
+package com.example.scienceguider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Liner_Motion_Functions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liner__motion__functions);
+    }
+}
