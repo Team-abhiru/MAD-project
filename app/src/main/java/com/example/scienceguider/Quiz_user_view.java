275f3f9430c03cb981085a7a8cfba975072cd218
Jayawardana JTH <tiran2323@gmail.com>
2020-03-16 19:34:00 +0530

    Create user Quiz view list type

---
+++ app/src/main/java/com/example/scienceguider/Quiz_user_view.java
@@ -0,0 +1,14 @@
+package com.example.scienceguider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz_user_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_user_view);
+    }
+}
