fa2c56bda5d71814d2aa1f9b218da1286bd103fb
Jayawardana JTH <tiran2323@gmail.com>
2020-03-16 20:52:50 +0530

    Remove activity to show questions to user as a list

--- app/src/main/java/com/example/scienceguider/MainActivity.java
+++ app/src/main/java/com/example/scienceguider/MainActivity.java
@@ -9,17 +9,12 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String questions [] = {"question 1", "question 2", "question 3"};
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_quiz_user_view, R.id.id_quiz,questions);
-
-        listView.findViewById(R.id.id_quiz_list);
-        listView.setAdapter(arrayAdapter);
     }
 }
