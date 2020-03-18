8c397b1843cd31f67c2ac4dd3c7380ffcf021e9c
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2020-03-18 19:40:17 +0530

    Changed

--- app/src/main/java/com/example/scienceguider/ChemistryActivity.java
+++ app/src/main/java/com/example/scienceguider/ChemistryActivity.java
@@ -3,12 +3,44 @@ package com.example.scienceguider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ChemistryActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chemistry);
+
+        listView = (ListView) findViewById(R.id.phyListView);
+
+        final ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Matter in our Surroundings");
+        arrayList.add("Is Matter Around us Pure");
+        arrayList.add("Atoms & Molecules");
+        arrayList.add("Structure of the Atom");
+        arrayList.add("Chemical Reactions and Equations");
+        arrayList.add("Acids, Bases, and Salts");
+        arrayList.add("Materials: Metals and Non-metals 1");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ChemistryActivity.this,"Clicked "
+                        +arrayList.get(i).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
