38697923408e3c4cd841b2756d06bec3ec513052
Jayawardana JTH <tiran2323@gmail.com>
2020-04-22 15:03:52 +0530

    Setup firebase database configurations

--- app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
+++ app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
@@ -1,10 +1,21 @@
 package com.example.scienceguider;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Add_new_Quiz extends AppCompatActivity {
+
+    EditText add_question;
+    EditText add_answer;
+    Button save;
+    Spinner subject;
+    Spinner topic;
+
+//    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/scienceguider/Temp.java
@@ -0,0 +1,57 @@
+package com.example.scienceguider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Temp extends AppCompatActivity {
+    Button attempt_quiz;
+    Button admin_quiz_view;
+    Button add_quiz;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+
+        attempt_quiz = (Button) findViewById(R.id.btn_attemt_quiz);
+        admin_quiz_view = (Button) findViewById(R.id.btn_admin_quiz);
+        add_quiz = (Button) findViewById(R.id.id_add_quiz);
+
+        admin_quiz_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                showQuizzes();
+            }
+        });
+
+        attempt_quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptQuiz();
+            }
+        });
+
+        add_quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addQuiz();
+            }
+        });
+    }
+
+    private void addQuiz() {
+        startActivity(new Intent(Temp.this,Add_new_Quiz.class));
+    }
+
+    private void showQuizzes() {
+        startActivity(new Intent(Temp.this,Quiz_list_user_view.class));
+    }
+
+    private void attemptQuiz() {
+
+        startActivity(new Intent(Temp.this,Quiz_list_user_view.class));
+    }
+}
