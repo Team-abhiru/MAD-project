b0644a6e31f7b9539b928f44c31ca1b4b9289655
Jayawardana JTH <tiran2323@gmail.com>
2020-04-22 15:31:37 +0530

    Add firebase database instance and libraries

--- app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
+++ app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class Add_new_Quiz extends AppCompatActivity {
+
+    DatabaseReference refDB;
 
     EditText add_question;
     EditText add_answer;
@@ -21,5 +27,24 @@ public class Add_new_Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new__quiz);
+
+        refDB = FirebaseDatabase.getInstance().getReference();
+
+        add_question = (EditText) findViewById(R.id.id_write_question);
+        add_answer = (EditText) findViewById(R.id.id_add_answer);
+        save = (Button) findViewById(R.id.id_btn_save);
+        subject = (Spinner) findViewById(R.id.spinner);
+        topic = (Spinner) findViewById(R.id.spinner2);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = add_question.getText().toString();
+                String answer = add_answer.getText().toString();
+
+                String selected_subject = subject.getSelectedItem().toString();
+                String selected_topic = topic.getSelectedItem().toString();
+            }
+        });
     }
 }
