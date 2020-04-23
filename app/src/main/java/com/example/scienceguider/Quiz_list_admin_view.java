49847f25845cb168faf7689bafa6f68c6f4eacc2
Jayawardana JTH <tiran2323@gmail.com>
2020-04-23 23:05:09 +0530

    Create method to show question list to admin

--- app/src/main/java/com/example/scienceguider/Quiz_list_admin_view.java
+++ app/src/main/java/com/example/scienceguider/Quiz_list_admin_view.java
@@ -1,29 +1,64 @@
 package com.example.scienceguider;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quiz_list_admin_view extends AppCompatActivity {
 
-    ListView listView;
+    private Question questionArray = null;
+    private ListView listView;
 
+    private List<Question> questionList;
+
+    private DatabaseReference refDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_list_admin_view);
 
-        String [] questions = {"Question 1", "Question 2", "Question 3", "Question 4"};
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_quiz_admin_view, R.id.id_quize_admin, questions);
+        refDB = FirebaseDatabase.getInstance().getReference("Questions");
 
         listView = findViewById(R.id.id_quiz_list_admin);
-        listView.setAdapter(arrayAdapter);
 
+        questionList = new ArrayList<>();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        refDB.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("GetValues", "Get values to an array");
+                questionList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    questionArray = snapshot.getValue(Question.class);
+                    questionList.add(questionArray);
+                }
+                QuestionList adapter = new QuestionList(Quiz_list_admin_view.this,questionList);
+
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("Error");
+            }
+        });
     }
 }
