f19e928b64e3facec5eba7e945112524f59a52b3
Jayawardana JTH <tiran2323@gmail.com>
2020-04-23 16:53:32 +0530

    Create attempt quiz get questions, check answer, get score and show answer

--- app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
+++ app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
@@ -32,7 +32,7 @@ public class Add_new_Quiz extends AppCompatActivity {
         setContentView(R.layout.activity_add_new__quiz);
 
         Log.d("started","App Started");
-        refDB = FirebaseDatabase.getInstance().getReference();
+        refDB = FirebaseDatabase.getInstance().getReference("Questions");
 
         add_question = (EditText) findViewById(R.id.id_write_question);
         add_answer = (EditText) findViewById(R.id.id_add_answer);
@@ -46,32 +46,27 @@ public class Add_new_Quiz extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Question questionObj = new Question();
 
                 String question = add_question.getText().toString();
-                String answer = add_answer.getText().toString();
+                int answer = Integer.parseInt(add_answer.getText().toString());
                 String option1 = answer1.getText().toString();
                 String option2 = answer2.getText().toString();
                 String option3 = answer3.getText().toString();
                 String selected_subject = subject.getSelectedItem().toString();
                 String selected_topic = topic.getSelectedItem().toString();
 
-                if(option1.isEmpty() || option2.isEmpty() || option3.isEmpty() || answer.isEmpty()
-                        || answer.isEmpty() || selected_subject.isEmpty()){
+
+                if(option1.isEmpty() || option2.isEmpty() || option3.isEmpty()
+                        || answer == 0 || selected_subject.isEmpty()){
 
                     Toast.makeText(Add_new_Quiz.this,"Fields cannot be empty",Toast.LENGTH_SHORT).show();
                 }
                 else{
 
-                    questionObj.setQuestion(question);
-                    questionObj.setAnswer(Integer.parseInt(answer));
-                    questionObj.setOption1(option1);
-                    questionObj.setOption2(option2);
-                    questionObj.setOption3(option3);
-                    questionObj.setSubject(selected_subject);
-                    questionObj.setTopic(selected_topic);
+                    Question questionObj = new Question(question, answer, option1, option2, option3,selected_subject, selected_topic );
 
-                    refDB.child("Questions").push().setValue(questionObj);
+                    String questionID = refDB.push().getKey();
+                    refDB.child(questionID).setValue(questionObj);
 
                     Toast.makeText(Add_new_Quiz.this,"New question is added",Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/com/example/scienceguider/Quiz_list_user_view.java
+++ app/src/main/java/com/example/scienceguider/Quiz_list_user_view.java
@@ -1,10 +1,15 @@
 package com.example.scienceguider;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Quiz_list_user_view extends AppCompatActivity {
@@ -22,31 +28,39 @@ public class Quiz_list_user_view extends AppCompatActivity {
     private TextView question;
     private TextView marks;
     private TextView subject;
-    private TextView topic;
+    private TextView quesNumber;
     private TextView counter;
     private RadioGroup radioGroup;
     private RadioButton option1, option2, option3;
     private Button confirm;
 
-    private int all_questions;
-    private int complete_questions;
+    private int questionsCount = 0;
+    private int questionNum = 0;
     private int score;
     private int countdown;
 
+    private ColorStateList defaultColor;
+    private Question currQues;
+    private Question questionArray = null;
+    private Boolean answered;
     private List<Question> questionList;
-    DatabaseReference refDB;
+    private DatabaseReference refDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_list_user_view);
 
+        Log.i("State","Create");
+
         refDB = FirebaseDatabase.getInstance().getReference("Questions");
+
+        questionList = new ArrayList<>();
 
         question = (TextView) findViewById(R.id.id_question);
         marks = (TextView) findViewById(R.id.id_score);
         subject = (TextView) findViewById(R.id.id_subject);
-        topic = (TextView) findViewById(R.id.id_subject_topic);
+        quesNumber = (TextView) findViewById(R.id.id_question_number);
         counter = (TextView) findViewById(R.id.id_counter);
 
         radioGroup = (RadioGroup) findViewById(R.id.id_radio_group);
@@ -56,18 +70,37 @@ public class Quiz_list_user_view extends AppCompatActivity {
 
         confirm = (Button) findViewById(R.id.id_btn_confirm);
 
+        defaultColor = option1.getTextColors();
+
         refDB.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Question question = null;
-                question = dataSnapshot.getValue(Question.class);
-
-                question.setQuestion(question.getQuestion());
-                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
-                    System.out.println(question.getQuestion());
-                    System.out.println(question.getOption1());
-                    System.out.println(question.getOption3());
+                Log.i("GetValues", "Get values to an array");
+                questionList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    questionArray = snapshot.getValue(Question.class);
+                    questionList.add(questionArray);
                 }
+                questionsCount = questionList.size();
+                getQuestion();
+
+                confirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!answered){
+                            if(option1.isChecked() || option2.isChecked() || option3.isChecked()){
+                                checkAnswer();
+                            }else{
+                                Toast.makeText(Quiz_list_user_view.this,"Select a answer",Toast.LENGTH_SHORT);
+                            }
+
+                        }else{
+                            getQuestion();
+
+                        }
+                    }
+                });
 
             }
 
@@ -77,4 +110,93 @@ public class Quiz_list_user_view extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("State","Start");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("State","Resume");
+        question.setText("Hello");
+    }
+
+    private void getQuestion(){
+        Log.i("State","question");
+        System.out.println(questionsCount);
+        if(questionNum < questionsCount){
+            option1.setTextColor(defaultColor);
+            option2.setTextColor(defaultColor);
+            option3.setTextColor(defaultColor);
+
+            radioGroup.clearCheck();
+
+            currQues = questionList.get(questionNum);
+
+            question.setText(currQues.getQuestion());
+            option1.setText(currQues.getOption1());
+            option2.setText(currQues.getOption2());
+            option3.setText(currQues.getOption3());
+
+            questionNum++;
+            quesNumber.setText("Question : " + questionNum);
+            answered = false;
+            confirm.setText("Confirm Answer");
+        }
+        else{
+            confirm.setText("Finish");
+            confirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finishQuiz();
+                }
+            });
+
+        }
+    }
+
+    private void checkAnswer(){
+        answered = true;
+
+        int answer = currQues.getAnswer();
+        RadioButton givenAnsButton = findViewById(radioGroup.getCheckedRadioButtonId());
+        int givenAns = radioGroup.indexOfChild(givenAnsButton) +1;
+
+        if(givenAns == answer){
+            score++;
+            marks.setText(("Marks : "+ score +"/"+questionsCount));
+        }
+
+        showSolution();
+
+    }
+
+    private void showSolution(){
+        int answer = currQues.getAnswer();
+
+        option1.setTextColor(Color.RED);
+        option2.setTextColor(Color.RED);
+        option3.setTextColor(Color.RED);
+
+        switch (answer){
+            case 1 :
+                option1.setTextColor(Color.GREEN);
+                break;
+            case 2 :
+                option2.setTextColor(Color.GREEN);
+                break;
+            case 3 :
+                option3.setTextColor(Color.GREEN);
+                break;
+        }
+        confirm.setText("Next");
+    }
+    private void finishQuiz(){
+        finish();
+    }
 }
+
