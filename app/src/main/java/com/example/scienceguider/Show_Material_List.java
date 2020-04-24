30dc3a188e4e6a52b040189e763066f467fd1f5d
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2020-04-24 21:23:18 +0530

    changed

--- app/src/main/java/com/example/scienceguider/Show_Material_List.java
+++ app/src/main/java/com/example/scienceguider/Show_Material_List.java
@@ -1,49 +1,100 @@
 package com.example.scienceguider;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.icu.text.Transliterator;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.errorprone.annotations.SuppressPackageLocation;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Show_Material_List extends AppCompatActivity {
 
     ListView listView;
-    TextView Mat_name;
+    List<PDF_Uploader> materialList;
+
+    DatabaseReference databaseReference;
+    FirebaseStorage firebaseStorage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show__material__list);
 
-        Mat_name = findViewById(R.id.mat_name);
-        listView = (ListView) findViewById(R.id.phyListView);
+        listView = (ListView) findViewById(R.id.ListView);
+        materialList = new ArrayList<>();
 
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("Mat_name");
+        viewAllFiles();
+
+    }
+
+    private void viewAllFiles() {
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Materials");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+
+                    PDF_Uploader uploader = postSnapshot.getValue(PDF_Uploader.class);
+                    materialList.add(uploader);
+
+                }
+
+                String[] uploads = new String[materialList.size()];
+
+                for (int i = 0; i < uploads.length; i++ ){
+
+                    uploads[i] = materialList.get(i).getTopic();
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,uploads);
+
+                listView.setAdapter(adapter);
+
+                viewAllFiles();
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        PDF_Uploader uploader = materialList.get(position);
+
+                        Intent intent = new Intent();
+                        intent.setType(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(uploader.getUrl()));
+                        startActivity(intent);
+                    }
+                });
 
 
-        Mat_name.setText(name);
+            }
 
-//        final ArrayList<String> arrayList = new ArrayList<>();
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
 
-//        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
-
-//        listView.setAdapter(arrayAdapter);
-
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(BiologyActivity.this,"Clicked "
-//                        +arrayList.get(i).toString(),Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 }
