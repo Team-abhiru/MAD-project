10212f2438d6b939a03e74fe73bca47c3ce38127
malith16 <malithdidulantha97@gmail.com>
2020-04-24 20:48:18 +0530

    register file connect with database

--- app/src/main/java/com/example/scienceguider/RegisterActivity.java
+++ app/src/main/java/com/example/scienceguider/RegisterActivity.java
@@ -1,25 +1,82 @@
 package com.example.scienceguider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText username,name,email,school,password;
+    EditText Musername,Mname,Memail,Mschool,Mpassword;
     Button register;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        username = findViewById(R.id.username);
-        name = findViewById(R.id.name);
-        email = findViewById(R.id.email);
-        school = findViewById(R.id.school);
-        password = findViewById(R.id.password);
+
+        Musername = findViewById(R.id.username);
+        Mname = findViewById(R.id.name);
+        Memail = findViewById(R.id.email);
+        Mschool = findViewById(R.id.school);
+        Mpassword = findViewById(R.id.password);
         register = findViewById(R.id.register);
+
+        fAuth = FirebaseAuth.getInstance();
+
+//        if (fAuth.getCurrentUser() != null){
+//            startActivity(new Intent(getApplicationContext(),Login.class));
+//            finish();
+//        }
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Memail.getText().toString().trim();
+                String password = Mpassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Memail.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Mpassword.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length()  <  6){
+                    Mpassword.setError("Password Must be >=6 characters");
+                    return;
+                }
+
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),Login.class));
+
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Error !!!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
