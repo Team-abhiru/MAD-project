3eee04b75c23f8ff54c7838ef500ab6bccf3d2fb
Jayawardana JTH <tiran2323@gmail.com>
2020-04-27 20:46:40 +0530

    Finalize add question fields checking

--- app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
+++ app/src/main/java/com/example/scienceguider/Add_new_Quiz.java
@@ -48,7 +48,7 @@ public class Add_new_Quiz extends AppCompatActivity {
             public void onClick(View v) {
 
                 String question = add_question.getText().toString();
-                int answer = Integer.parseInt(add_answer.getText().toString());
+                String refAnswer = add_answer.getText().toString();
                 String option1 = answer1.getText().toString();
                 String option2 = answer2.getText().toString();
                 String option3 = answer3.getText().toString();
@@ -56,12 +56,15 @@ public class Add_new_Quiz extends AppCompatActivity {
                 String selected_topic = topic.getSelectedItem().toString();
 
 
+
                 if(option1.isEmpty() || option2.isEmpty() || option3.isEmpty()
-                        || answer == 0 || selected_subject.isEmpty()){
+                        || refAnswer.isEmpty() || question.isEmpty() || selected_subject.contentEquals("Subject")
+                        || selected_topic.contentEquals("Topic")){
 
                     Toast.makeText(Add_new_Quiz.this,"Fields cannot be empty",Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    int answer = Integer.parseInt(add_answer.getText().toString());
 
                     Question questionObj = new Question(question, answer, option1, option2, option3,selected_subject, selected_topic );
 
